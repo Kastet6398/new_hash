3c4ce923a1414df4cc0d38cf45ef4503ee89fe2c
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-07-17 20:02:38 +0300

    updated requirements.txt, crypto_lib.py, test_crypto_lib.py

--- crypto_lib.py
+++ crypto_lib.py
@@ -1,6 +1,25 @@
 import hashlib
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def encode_md5(value: str) -> str:
     result = hashlib.md5(value.encode()).hexdigest()
     return result
+
+
+def get_password_hash(password: str) -> str:
+    result = pwd_context.hash(password)
+    return result
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    result = pwd_context.verify(plain_password, hashed_password)
+    return result
+
+
+password_hash = get_password_hash("1")
+verified_password = verify_password("1", password_hash)
+print(password_hash, verified_password)
