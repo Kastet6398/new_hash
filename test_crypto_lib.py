2c20d478a84452c99e559a14185b347584b75467
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-07-17 19:29:24 +0300

    update test_crypto_lib.py

--- test_crypto_lib.py
+++ test_crypto_lib.py
@@ -5,6 +5,7 @@ import crypto_lib
 test_cases_md5 = [
     ("qwerty", "d8578edf8458ce06fbc5bb76a58c5ca4"),
     ('srjtyusryt734476583574', '127020bf98da7b018bc6deb1633d21a3'),
+    ('www.facebook.com', '660328a7f9004d462085aa67a82065db'),
 ]
 
 
