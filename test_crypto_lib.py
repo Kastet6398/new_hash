966af45401b23ba281f622e4b72ee9fd63f30970
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-07-17 19:25:41 +0300

    change test_crypto_lib.py

--- test_crypto_lib.py
+++ test_crypto_lib.py
@@ -2,8 +2,10 @@ import pytest
 
 import crypto_lib
 
-test_cases_md5 = [("qwerty", "d8578edf8458ce06fbc5bb76a58c5ca4"),
-                  ('srjtyusryt734476583574', '127020bf98da7b018bc6deb1633d21a3')]
+test_cases_md5 = [
+    ("qwerty", "d8578edf8458ce06fbc5bb76a58c5ca4"),
+    ('srjtyusryt734476583574', '127020bf98da7b018bc6deb1633d21a3'),
+]
 
 
 @pytest.mark.parametrize('value, expected', test_cases_md5)
